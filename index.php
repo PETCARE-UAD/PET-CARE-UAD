563961cba7c9eda6d4ede40ab947859e64a809d5
Meilinbudiarti <62578816+Meilinbudiarti@users.noreply.github.com>
2020-05-02 22:32:54 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,250 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <title>PET CARE UAD</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,700,800" rel="stylesheet">
+
+    <!-- Theme Style -->
+    <link rel="stylesheet" href="css/style.css">
+  </head>
+  <body>
+    
+    <header role="banner">
+      <nav class="navbar navbar-expand-md navbar-dark bg-dark">
+        <div class="container">
+          <a class="navbar-brand" href="latihan2.html"></a>
+          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExample05" aria-controls="navbarsExample05" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+
+          <div class="collapse navbar-collapse" id="navbarsExample05">
+            <ul class="navbar-nav ml-auto pl-lg-5 pl-0">
+              <li class="nav-item">
+                <a class="nav-link active" href="latihan2.html">Home</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="latihan2.html">Download</a>
+              </li>
+              <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="latihan2.html" id="dropdown04" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">PET CARE STORY</a>
+                <div class="dropdown-menu" aria-labelledby="dropdown04">
+                  <a class="dropdown-item" href="latihan2.html"></a>
+                  <a class="dropdown-item" href="latihan2.html">HOME</a>
+                  <a class="dropdown-item" href="latihan2.html">HEWAN</a>
+                  <a class="dropdown-item" href="latihan2.html">PERAWATAN</a>
+                  <a class="dropdown-item" href="latihan2.html">PENITIPAN</a>
+                </div>
+              </li>
+            <ul class="navbar-nav ml-auto">
+              <li class="nav-item cta-btn">
+                <a class="nav-link" href="login.html">Login<font size="1px">(Signup)</font></a>
+              </li>
+            </ul>
+        
+          </div>
+        </div>
+      </nav>
+    </header>
+    <!-- END header -->
+    
+    <div class="slider-wrap">
+      <section class="home-slider owl-carousel">
+
+        <div class="slider-item" style="background-image: url('img/petcare.jpg');">
+          <div class="container">
+            <div class="row slider-text align-items-center justify-content-center">
+              <div class="col-md-8 text-center col-sm-12 ">
+                <h1 data-aos="fade-up">Layanan Hewan</h1>
+                <p class="mb-5" data-aos="fade-up" data-aos-delay="100">Kami Menyediakan Layanan Untuk Merawat dan Penitipan Hewan</p>
+              </div>
+            </div>
+          </div>
+          
+        </div>
+
+      </section>
+    <!-- END slider -->
+    </div> 
+    
+
+    <section class="section pb-0">
+      <div class="container">
+        <div class="row mb-5 justify-content-center" data-aos="fade">
+            <div class="col-md-7 text-center heading-wrap">
+              <h2 data-aos="fade-up">About</h2>
+              <p data-aos="fade-up" data-aos-delay="100">
+Aplikasi PET CARE UAD bertujuan untuk merawat dan menitipkan hewan kesayangan anda dimana anda bisa menikmati layanan yang begitu mantap</p>
+            </div>
+          </div>
+        <div class="row align-items-center">
+          <div class="col-lg-4">
+            <img src="img/anjing1.jpg" alt="Image" class="img-fluid about_img_1" data-aos="fade" data-aos-delay="200">
+          </div>
+          <div class="col-lg-4">
+            <img src="img/kucing.jpg" alt="Image" class="img-fluid about_img_1" data-aos="fade" data-aos-delay="400">
+          </div>
+          <div class="col-lg-4">
+            <img src="img/hamster.jpg" alt="Image" class="img-fluid about_img_1" data-aos="fade" data-aos-delay="500">
+          </div>
+          <div class="col-lg-4">
+            <img src="img/kelinci.jpg" alt="Image" class="img-fluid about_img_1" data-aos="fade" data-aos-delay="500">
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <section class="section ">
+
+      <div class="clearfix mb-5 pb-5">
+        <div class="container-fluid mb-5">
+          <div class="row" data-aos="fade">
+            <div class="col-md-12 text-center heading-wrap">
+              <h2>Perawatan Hewan</h2>
+            </div>
+          </div>
+        </div>
+        <div class="owl-carousel centernonloop">
+          <a href="#" class="item-dishes" data-aos="fade-right" data-aos-delay="100">
+            <div class="text">
+              <p class="dishes-price">Rp.200.000</p>
+              <h2 class="dishes-heading">Anjing</h2>
+            </div>
+            <img src="img/anjing1.jpg" alt="" class="img-fluid">
+          </a>
+          <a href="#" class="item-dishes" data-aos="fade-right" data-aos-delay="300">
+            <div class="text">
+              <p class="dishes-price">Rp.150.000</p>
+              <h2 class="dishes-heading">Kucing</h2>
+            </div>
+            <img src="img/kucing.jpg" alt="" class="img-fluid">
+          </a>
+          <a href="#" class="item-dishes" data-aos="fade-right" data-aos-delay="400">
+            <div class="text">
+              <p class="dishes-price">Rp.100.000</p>
+              <h2 class="dishes-heading">Hamster</h2>
+            </div>
+            <img src="img/hamster.jpg" alt="" class="img-fluid">
+          </a>
+          <div class="text">
+              <p class="dishes-price">Rp.80.000</p>
+              <h2 class="dishes-heading">Hamster</h2>
+            </div>
+            <img src="img/kelinci.jpg" alt="" class="img-fluid">
+          </a>
+        </div>
+      </div>
+
+    </section> <!-- .section -->
+
+    <section class="section bg-light  top-slant-white bottom-slant-gray">
+
+      <div class="clearfix mb-5 pb-5">
+        <div class="container-fluid">
+          <div class="row" data-aos="fade">
+            <div class="col-md-12 text-center heading-wrap">
+              <h2>PET CARE UAD</h2>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="container">
+        
+        <div class="row no-gutters">
+          <div class="col-md-6">
+            <div class="sched d-block d-lg-flex">
+              <div class="bg-image order-2" style="background-image: url('img/anjing3.jpg');" data-aos="fade"></div>
+              <div class="text order-1">
+              </div>
+              
+            </div>
+
+            <div class="sched d-block d-lg-flex">
+              <div class="bg-image" style="background-image: url('img/kuciing2.jpg');" data-aos="fade"></div>
+              <div class="text">
+              </div>
+              
+            </div>
+
+          </div>
+
+          <div class="col-md-6">
+            <div class="sched d-block d-lg-flex">
+              <div class="bg-image order-2" style="background-image: url('img/hamster3.jpg');" data-aos="fade"></div>
+              <div class="text order-1">
+              </div>
+              
+            </div>
+
+            <div class="sched d-block d-lg-flex">
+              <div class="bg-image" style="background-image: url('img/kelinci1.jpg');" data-aos="fade"></div>
+              <div class="text">
+              </div>
+              
+            </div>
+
+          </div>
+        </div>
+      </div>
+    </section> <!-- .section -->
+
+  
+
+    <footer class="site-footer" role="contentinfo">
+      <div class="container">
+        <div class="row mb-5">
+          <div class="col-md-4 mb-5">
+            <h3>PET CARE STORY</h3>
+            <p class="mb-5">PET CARE UAD bertujuan untuk merawat dan menitipkan hewan kesayangan anda dimana anda bisa menikmati layanan yang begitu mantap</p>
+            <ul class="list-unstyled footer-link d-flex footer-social">
+              <li><a href="#" class="p-2"><span class="fa fa-twitter"></span></a></li>
+              <li><a href="#" class="p-2"><span class="fa fa-facebook"></span></a></li>
+              <li><a href="#" class="p-2"><span class="fa fa-linkedin"></span></a></li>
+              <li><a href="#" class="p-2"><span class="fa fa-instagram"></span></a></li>
+            </ul>
+
+          </div>
+          <div class="col-md-5 mb-5">
+            <div>
+              <h3>Contact Info</h3>
+              <ul class="list-unstyled footer-link">
+                <li class="d-block"><span class="d-block text-black">Phone:</span><span>0821 7590 3800</span></li>
+                <li class="d-block"><span class="d-block text-black">Email:</span><span>hamimarsy@gmail.com</span></li>
+              </ul>
+            </div>
+          </div>
+          <div class="col-md-3">
+          
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-12 text-md-center text-left">
+            <p>
+            Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved to my team<i class="fa fa-heart text-danger" aria-hidden="true"></i>
+            </p>
+          </div>
+        </div>
+      </div>
+    </footer>
+    <!-- END footer -->
+
+    <!-- loader -->
+    <div id="loader" class="show fullscreen"><svg class="circular" width="48px" height="48px"><circle class="path-bg" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke="#eeeeee"/><circle class="path" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke-miterlimit="10" stroke="#cf1d16"/></svg></div>
+
+    <script src="js/jquery-3.2.1.min.js"></script>
+    <script src="js/popper.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/owl.carousel.min.js"></script>
+    <script src="js/jquery.waypoints.min.js"></script>
+    <script src="js/aos.js"></script>
+
+    <script src="js/jquery.magnific-popup.min.js"></script>
+    <script src="js/magnific-popup-options.js"></script>
+    
+
+    <script src="js/main.js"></script>
+    
+  </body>
+</html>
