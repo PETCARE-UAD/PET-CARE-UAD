70541b42a48c95d41373e70eba891c7aeb1f7dfb
hamimarsy <62856670+hamimarsy@users.noreply.github.com>
2020-05-02 12:52:53 +0800

    menampilkan halaman utama

--- home.php
+++ home.php
@@ -1,6 +1,6 @@
 <!doctype html>
 <html lang="en">
-  <head>
+<head>
     
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
@@ -9,8 +9,9 @@
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
 
     <title>PET CARE UAD</title>
-  </head>
-  <body>
+
+</head>
+<body>
     
   <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
     <img src="img/logoo.jpg" width="50">
