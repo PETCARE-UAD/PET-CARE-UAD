08526b54f2abd39adb43e1ff822a1eff7c28fda8
hamimarsy <62856670+hamimarsy@users.noreply.github.com>
2020-05-02 12:51:18 +0800

    Update home.php

--- home.php
+++ home.php
@@ -12,7 +12,7 @@
   </head>
   <body>
     
-    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
     <img src="img/logoo.jpg" width="50">
         <a class="navbar-brand"> PETCare UAD</a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
