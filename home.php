11936d334ac22a51a93feb39d29fdfe4e24242a4
hamimarsy <62856670+hamimarsy@users.noreply.github.com>
2020-05-02 12:44:43 +0800

    Add files via upload

---
+++ home.php
@@ -0,0 +1,65 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+
+    <title>PET CARE UAD</title>
+  </head>
+  <body>
+    
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <img src="img/logoo.jpg" width="50">
+        <a class="navbar-brand"> PETCare UAD</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+      
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav mr-auto">
+            <li class="nav-item active">
+              <a class="nav-link" href="">Home</a>
+            </li>
+            <li class="nav-item ">
+              <a class="nav-link" href="">About us</a>
+            </li>
+            <li class="nav-item ">
+              <a class="nav-link" href="registrasi.php">Registrasi <span class="sr-only"></span></a>
+            </li>
+            <li class="nav-item dropdown">
+              <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                Menu
+              </a>
+              <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                <a class="dropdown-item" href="#">Penitipan</a>
+                <a class="dropdown-item" href="#">Perawatan</a>
+                <a class="dropdown-item" href="#">Mandikan</a>
+                <a class="dropdown-item" href="login.php">login</a>
+                <div class="dropdown-divider"></div>
+                <a class="dropdown-item" href="">logout</a>
+              </div>
+            </li>
+            
+        </div>
+      </nav>
+      
+    <div class="container mt-3">
+      <div class="jumbotron jumbotron-fluid">
+        <div class="container text-center">
+            <img src="img/hewan.jpg" width="500" >
+          <h1 class="display-4">SELAMAT DATANG DI PET CARE UAD</h1>
+          <p class="lead">Melayani Jasa Penitipan dan Perawatan Hewan Peliharaan Dengan Sangat Baik</p>
+        </div>
+        </div>
+    </div>
+    </div>
+    
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+  </body>
+</html>
