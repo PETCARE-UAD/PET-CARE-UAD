730fce56c0d2fd032789755f89781d21b58d5b37
Meilinbudiarti <62578816+Meilinbudiarti@users.noreply.github.com>
2020-07-30 19:42:49 +0700

    Add files via upload

--- koneksi.php
+++ koneksi.php
@@ -1,12 +1,8 @@
-<?php
-$koneksi = mysqli_connect('localhost','root','','petcare1');
-
-/*if($koneksi){
-	echo "koneksi berhasil";
+<?php 
+$koneksi = mysqli_connect("localhost","root","","db_petcare2");
+ 
+if (mysqli_connect_errno()){
+	echo "Koneksi database gagal : " . mysqli_connect_error();
 }
-else {
-	echo "koneksi gagal";
-}
-*/
-
+ 
 ?>
